0fbed65cc16b07dd70be142b7d666a27e245c58f
Seb Boyse <seb.boyse@korelogic.co.uk>
2026-05-16 20:58:00 +0100

    Updates

---
+++ assets/allPathsLoader-C-VdcK2m.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-X-5kf4uM.js","assets/index-DtzT8OsX.js","assets/index-mbPvlb_I.js","assets/index-DeOh8T2t.js","assets/index-Cfm-BHpY.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-DeOh8T2t.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-X-5kf4uM.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]));return o(t,a)};export{s as allPathsLoader};
