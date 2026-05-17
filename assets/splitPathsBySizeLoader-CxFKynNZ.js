46ae3335c1774eb8f8e0bacd87aaf268f824766a
Seb Boyse <seb.boyse@korelogic.co.uk>
2026-05-17 17:43:30 +0100

    Updates

---
+++ assets/splitPathsBySizeLoader-CxFKynNZ.js
@@ -0,0 +1,1 @@
+import{p as r,I as s,_ as a}from"./index-DHu0Tgad.js";const n=async(o,_)=>{const i=r(o);let t;return _===s.STANDARD?t=await a(()=>import("./index-DtzT8OsX.js").then(e=>e.I),[]):t=await a(()=>import("./index-mbPvlb_I.js").then(e=>e.I),[]),t[i]};export{n as splitPathsBySizeLoader};
