8948c54f2a54fcaa259c3a389150274ef9e77d6c
Seb Boyse <seb.boyse@korelogic.co.uk>
2026-05-17 15:33:55 +0100

    Updates

---
+++ assets/allPathsLoader-ClXilE6R.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-B6QT36Sc.js","assets/index-DtzT8OsX.js","assets/index-mbPvlb_I.js","assets/index-CIELkPaZ.js","assets/index-eqsGBPBc.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-CIELkPaZ.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-B6QT36Sc.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]));return o(t,a)};export{s as allPathsLoader};
--- assets/splitPathsBySizeLoader-BcZRvqi3.js
+++
@@ -1,1 +0,0 @@
-import{p as r,I as s,_ as a}from"./index-BaWHPTE7.js";const n=async(o,_)=>{const i=r(o);let t;return _===s.STANDARD?t=await a(()=>import("./index-DtzT8OsX.js").then(e=>e.I),[]):t=await a(()=>import("./index-mbPvlb_I.js").then(e=>e.I),[]),t[i]};export{n as splitPathsBySizeLoader};
